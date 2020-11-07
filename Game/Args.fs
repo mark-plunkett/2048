7a8afbcf6d2d43fa491533312a2deb0c0050791f
mark.plunkett <markje.p@gmail.com>
2020-11-07 22:52:29 +0000

    Updated args messages.

--- Game/Args.fs
+++ Game/Args.fs
@@ -10,6 +10,6 @@ type Args =
     interface IArgParserTemplate with
         member s.Usage =
             match s with
-            | Human -> "specify human solver (interactive)."
+            | Human -> "specify human solver (interactive), default."
             | MonteCarlo _ -> "specify monte-carlo 'AI' solver with number of branches and branch depth."
-            | Size _ -> "specify board size."
+            | Size _ -> "specify board size, default = 4."
