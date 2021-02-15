4023310ffd95e7147ca85f044c777c3bfebe5b23
mark.plunkett <markje.p@gmail.com>
2021-02-15 21:18:44 +0000

    Updated benchmarking to include GameArray.

--- Benchmark/Benchmarks.fs
+++ Benchmark/Benchmarks.fs
@@ -45,25 +45,37 @@ type Benchmarks () =
     member val public numRuns = 0 with get, set
 
     [<Benchmark>]
-    member this.RunMovesSeq () =
+    member this.RunMovesFunctionalSeq () =
         let board = contextFunctional.CreateWithSeed size seed
         let dirFactory = MonteCarloSolver.genNextDir monteNumBranches monteNumMoves contextFunctional
         runLoop dirFactory this.numRuns contextFunctional board
 
     [<Benchmark>]
-    member this.RunMovesPSeq () =
+    member this.RunMovesFunctionalPSeq () =
         let board = contextFunctional.CreateWithSeed size seed
         let dirFactory = MonteCarloSolver.genNextDirPSeq monteNumBranches monteNumMoves contextFunctional
         runLoop dirFactory this.numRuns contextFunctional board
 
     [<Benchmark>]
-    member this.RunMovesFastSeq () =
+    member this.RunMovesDictSeq () =
         let board = contextDict.CreateWithSeed size seed
         let dirFactory = MonteCarloSolver.genNextDir monteNumBranches monteNumMoves contextDict
         runLoop dirFactory this.numRuns contextDict board
 
     [<Benchmark>]
-    member this.RunMovesFastPSeq () =
+    member this.RunMovesDictPSeq () =
         let board = contextDict.CreateWithSeed size seed
         let dirFactory = MonteCarloSolver.genNextDirPSeq monteNumBranches monteNumMoves contextDict
         runLoop dirFactory this.numRuns contextDict board
+
+    [<Benchmark>]
+    member this.RunMovesArraySeq () =
+        let board = GameArray.boardContext.CreateWithSeed size seed
+        let dirFactory = MonteCarloSolver.genNextDir monteNumBranches monteNumMoves GameArray.boardContext
+        runLoop dirFactory this.numRuns GameArray.boardContext board
+
+    [<Benchmark>]
+    member this.RunMovesArrayPSeq () =
+        let board = GameArray.boardContext.CreateWithSeed size seed
+        let dirFactory = MonteCarloSolver.genNextDirPSeq monteNumBranches monteNumMoves GameArray.boardContext
+        runLoop dirFactory this.numRuns GameArray.boardContext board
