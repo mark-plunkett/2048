7b68b14da7b4043314511db1437c63c867bb2696
mark.plunkett <markje.p@gmail.com>
2021-12-29 11:00:27 +0000

    Avx shuffle poc.

--- ManualTests/AvxTests.fsx
+++ ManualTests/AvxTests.fsx
@@ -5,7 +5,41 @@ let x = 40
 
 printfn "%B" x
 
-let v1 = Vector256.Create(
+let firstOfEachPairMask = Vector256.Create(
+    0uy,
+    2uy,
+    4uy,
+    6uy,
+    8uy,
+    10uy,
+    12uy,
+    14uy,
+    16uy,
+    18uy,
+    20uy,
+    22uy,
+    24uy,
+    26uy,
+    28uy,
+    30uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy,
+    0uy)
+
+let cells1 = Vector256.Create(
     0s,
     1s,
     2s,
@@ -14,6 +48,18 @@ let v1 = Vector256.Create(
     5s,
     6s,
     7s,
+    0s,
+    0s,
+    0s,
+    0s,
+    0s,
+    0s,
+    0s,
+    0s)
+
+printfn "cells %A" cells1
+
+let cells2 = Vector256.Create(
     8s,
     9s,
     10s,
@@ -21,58 +67,90 @@ let v1 = Vector256.Create(
     12s,
     13s,
     14s,
-    15s)
+    15s,
+    0s,
+    0s,
+    0s,
+    0s,
+    0s,
+    0s,
+    0s,
+    0s)
 
-printfn "v1 %A" v1
+printfn "cells2 %A" cells2
 
-let v1Bytes = v1.AsSByte()
+let cells1Bytes = cells1.AsByte()
 
-printfn "v1b %A" v1Bytes
+printfn "cells1Bytes %A" cells1Bytes
 
-(*
-    [0 1 2 3]
-    [0 1]
-    [2 3]
+let cells1Packed = Avx2.Shuffle (cells1Bytes, firstOfEachPairMask)
 
-    rotate anti-clockwise
-    [1 3 0 2]
-    [1 3]
-    [0 2]
+printfn "cells1Packed %A" cells1Packed
 
-    [1 3 0 2]
+let cells2Bytes = cells2.AsByte()
+let cells2Packed = Avx2.Shuffle (cells2Bytes, firstOfEachPairMask)
 
-    [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15]
-    [0 1 2 3]
-    [4 5 6 7]
-    [8 9 10 11]
-    [12 13 14 15]
+printfn "cells2Packed %A" cells2Packed
 
-    rotate anti-clockwise
-    [ 3 7 11 15 2 6 10 14 1 5 9 13 0 4 8 12 ]
+let combined = Avx2.BlendVariable (
+    cells1Packed,
+    cells2Packed,
+    Vector256.Create(
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        System.Byte.MaxValue,
+        System.Byte.MaxValue,
+        System.Byte.MaxValue,
+        System.Byte.MaxValue,
+        System.Byte.MaxValue,
+        System.Byte.MaxValue,
+        System.Byte.MaxValue,
+        System.Byte.MaxValue,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy,
+        0uy))
 
-*)
+printfn "combined %A" combined
 
-let antiClockwiseMask = Vector256.Create(
-    6y, 7y,
-    14y, 15y,
-    22y, 23y,
-    30y, 31y,
-    4y, 5y,
-    12y, 13y,
-    20y, 21y,
-    28y, 29y,
-    2y, 3y,
-    10y, 11y,
-    18y, 19y,
-    26y, 27y,
-    0y, 1y,
-    8y, 9y,
-    16y, 17y,
-    24y, 25y)
+let final128 = combined.GetLower()
 
-let antiClockwiseMaskBytes = antiClockwiseMask.AsSByte()
-printfn "mask bytes %A" antiClockwiseMaskBytes
+let antiClockwiseMask = Vector128.Create(
+    3uy,
+    7uy,
+    11uy,
+    15uy,
+    2uy,
+    6uy,
+    10uy,
+    14uy,
+    1uy,
+    5uy,
+    9uy,
+    13uy,
+    0uy,
+    4uy,
+    8uy,
+    12uy)
 
-let res = Avx2.Shuffle (v1Bytes, antiClockwiseMaskBytes) |> Vector256.AsUInt16
+let res = Avx2.Shuffle (final128, antiClockwiseMask)
 
 printfn "res %A" res
