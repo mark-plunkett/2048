ccc04117708bc8ed142018ef33da9d134d8ae445
mark.plunkett <markje.p@gmail.com>
2023-03-04 10:24:40 +0000

    Updated benchmarks to include cache misses and branch mispredictions.

--- Benchmark/Benchmarks.fs
+++ Benchmark/Benchmarks.fs
@@ -1,13 +1,14 @@
 ﻿module Benchmark 
 
 open System
-open BenchmarkDotNet
+open BenchmarkDotNet.Diagnosers
 open BenchmarkDotNet.Attributes
 
 open Common
 
+[<HardwareCounters(HardwareCounter.BranchMispredictions, HardwareCounter.CacheMisses)>] 
 [<MemoryDiagnoser>]
-[<MarkdownExporterAttribute.GitHub>]
+// [<MarkdownExporterAttribute.GitHub>]
 type Benchmarks () =
 
     let numRuns = 1
@@ -16,8 +17,7 @@ type Benchmarks () =
     let monteNumMoves = 100
 
     let size = 4
-    let r = Random()
-    let seed = r.Next()
+    let seed = 123
 
     let rec runLoop dirFactory numRuns context board =
         if numRuns = 0 then
@@ -68,33 +68,3 @@ type Benchmarks () =
     [<Benchmark>]
     member this.SIMDBranchless () =
         runLoop dirFactorySIMDBranchless numRuns GameSIMDBranchless.boardContext boardSIMDBranchless
-
-    //[<Benchmark>]
-    member this.FunctionalPSeq () =
-        let board = GameFunctional.boardContext.CreateWithSeed size seed
-        let dirFactory = MonteCarloSolver.genNextDirPSeq monteNumBranches monteNumMoves GameFunctional.boardContext
-        runLoop dirFactory numRuns GameFunctional.boardContext board
-
-    //[<Benchmark>]
-    member this.DictPSeq () =
-        let board = GameDict.boardContext.CreateWithSeed size seed
-        let dirFactory = MonteCarloSolver.genNextDirPSeq monteNumBranches monteNumMoves GameDict.boardContext
-        runLoop dirFactory numRuns GameDict.boardContext board
-
-    //[<Benchmark>]
-    member this.ArrayPSeq () =
-        let board = GameArray.boardContext.CreateWithSeed size seed
-        let dirFactory = MonteCarloSolver.genNextDirPSeq monteNumBranches monteNumMoves GameArray.boardContext
-        runLoop dirFactory numRuns GameArray.boardContext board
-
-    //[<Benchmark>]
-    member this.SIMDPSeq () =
-        let board = GameSIMD.boardContext.CreateWithSeed size seed
-        let dirFactory = MonteCarloSolver.genNextDirPSeq monteNumBranches monteNumMoves GameSIMD.boardContext
-        runLoop dirFactory numRuns GameSIMD.boardContext board
-
-    //[<Benchmark>]
-    member this.SIMDPlusPSeq () =
-        let board = GameSIMDPlus.boardContext.CreateWithSeed size seed
-        let dirFactory = MonteCarloSolver.genNextDirPSeq monteNumBranches monteNumMoves GameSIMDPlus.boardContext
-        runLoop dirFactory numRuns GameSIMDPlus.boardContext board
--- Benchmark/Program.fs
+++ Benchmark/Program.fs
@@ -1,9 +1,7 @@
-﻿open System
-open BenchmarkDotNet.Running
+﻿open BenchmarkDotNet.Running
 open Benchmark
-open BuildIndexBenchmarks
 
 [<EntryPoint>]
 let main argv =
     BenchmarkRunner.Run<Benchmarks>() |> ignore
-    0 // return an integer exit code
+    0
